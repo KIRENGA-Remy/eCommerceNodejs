c6d004eae7f760ed5817260a8291480c7c245936
Remy_Claudien <gitoliremy.globalsimplilearn.net>
2023-11-22 07:27:47 +0200

    Debugging

--- Routes/loginRoute.js
+++ Routes/loginRoute.js
@@ -1,32 +1,34 @@
-const User = require("../model/Users.js");
-const bcrypt = require("bcrypt");
-const router = require("express").Router();
+const User = require('../model/Users.js');
+const bcrypt = require('bcrypt');
+const router = require('express').Router(); 
 
 router.post("/", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const eUser = await User.findOne({ email });
-    if (!eUser) {
-      res.send({
-        message: "The user doesn't exist, Please signup",
-        alert: false,
-      });
-    }
+    const { email, password } = req.body;
+    try{
+        const eUser = await User.findOne({email});
+        if(!eUser) {
+            res.send({ message: "The user doesn't exist, Please signup", alert: false });
+        }
 
-    const dataSend = {
-      _id: eUser._id,
-      firstName: eUser.firstName,
-      lastName: eUser.lastName,
-      email: eUser.email,
-      image: eUser.image,
-    };
+        if(!(bcrypt.compareSync(password, eUser.password))){
+            return res.status(403).json({message: 'Invalid password or email...'})
+        }
 
-    return res.status(201).json({
-      data: dataSend,
-    });
-  } catch (e) {
-    return res.status(500).json({ message: "Internal server error..." });
-  }
+        const dataSend = {
+            _id: eUser._id,
+            firstName: eUser.firstName,
+            lastName: eUser.lastName,
+            email: eUser.email,
+            image: eUser.image
+        };
+
+        return res.status(201).json({
+            data: dataSend
+        })
+       
+}catch(e) {
+    return res.status(500).json({message: 'Internal server error...'})
+}
 });
 
 module.exports = router;
