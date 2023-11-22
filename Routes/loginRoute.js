ca22a3c41fb522d3ec656982ea37bfcfcef4ca8d
Remy_Claudien <gitoliremy.globalsimplilearn.net>
2023-11-22 07:26:06 +0200

    Debugging

--- Routes/loginRoute.js
+++ Routes/loginRoute.js
@@ -1,33 +1,32 @@
-const User = require('../model/Users.js');
-const bcrypt = require('bcrypt');
-const router = require('express').Router(); 
+const User = require("../model/Users.js");
+const bcrypt = require("bcrypt");
+const router = require("express").Router();
 
 router.post("/", async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
+  try {
+    const eUser = await User.findOne({ email });
+    if (!eUser) {
+      res.send({
+        message: "The user doesn't exist, Please signup",
+        alert: false,
+      });
+    }
 
-    User.findOne({ email: email }, async (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: "Internal Server Error", alert: false });
-        }
-        if (result) {
-           
-            const checkCorrectPassword = await bcrypt.compare(password, result.password);
-            if (!checkCorrectPassword) {
-                return res.status(401).json({ message: "Incorrect Password, Try another." });
-            }
-            const dataSend = {
-                _id: result._id,
-                firstName: result.firstName,
-                lastName: result.lastName,
-                email: result.email,
-                image: result.image
-            };
-            console.log(dataSend);
-            res.send({ message:"User logged in successfully", alert: true, data: dataSend });
-        } else {
-            res.send({ message: "The user doesn't exist, Please signup", alert: false });
-        }
+    const dataSend = {
+      _id: eUser._id,
+      firstName: eUser.firstName,
+      lastName: eUser.lastName,
+      email: eUser.email,
+      image: eUser.image,
+    };
+
+    return res.status(201).json({
+      data: dataSend,
     });
+  } catch (e) {
+    return res.status(500).json({ message: "Internal server error..." });
+  }
 });
 
 module.exports = router;
