e39370282f49d220bb7d274f2811f1eda54ea50e
Remy_Claudien <gitoliremy.globalsimplilearn.net>
2023-11-22 06:07:56 +0200

    changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const signupRoute = require('./Routes/signupRoute.js');
-const loginRoute = require('./Routes/loginRoute.js');
-const productsRoute = require('./Routes/productsRoute.js');
-const getProducts = require('./Routes/getProducts.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const signupRoute = require("./Routes/signupRoute.js");
+const loginRoute = require("./Routes/loginRoute.js");
+const productsRoute = require("./Routes/productsRoute.js");
+const getProducts = require("./Routes/getProducts.js");
 
 dotenv.config();
 
@@ -15,32 +15,40 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 mongoose.set("strictQuery", false);
 
-const connect = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Successfully connected to the database');
-    } catch (error) {
-        console.error('Failed to connect to the database:', error);
-    }
-};
+// const connect = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URL, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('Successfully connected to the database');
+//     } catch (error) {
+//         console.error('Failed to connect to the database:', error);
+//     }
+// };
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected..."))
+  .catch((e) => console.log(e));
 
 // Move connect() before setting up routes
-connect();
+// connect();
 
-app.get('/', (req, res) => {
-    res.send('This server is connected');
+app.get("/", (req, res) => {
+  res.send("This server is connected");
 });
 
-app.use('/signup', signupRoute);
-app.use('/login', loginRoute);
-app.use('/products', productsRoute);
-app.use('/product', getProducts);
+app.use("/signup", signupRoute);
+app.use("/login", loginRoute);
+app.use("/products", productsRoute);
+app.use("/product", getProducts);
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`The server is running on http://localhost:${PORT}`);
+  console.log(`The server is running on http://localhost:${PORT}`);
 });
