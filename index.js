223bffa663590a7e4e0031609a87ad1f6366f930
Remy_Claudien <gitoliremy.globalsimplilearn.net>
2023-11-25 19:19:30 +0200

    minor changes

--- index.js
+++ index.js
@@ -79,5 +79,5 @@ const PORT = process.env.PORT || 8080;
 
 
 app.listen(PORT, () => {
-  console.log(`The server is running on http://localhost:${PORT}`);
+  console.log(`The Server is running on http://localhost:${PORT}`);
 });
